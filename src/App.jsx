338380f362513067503335b0d25c870431243596
Bruno Santos <brunosantosfarias2@gmail.com>
2022-02-28 12:01:29 -0300

    ci: add workflow

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Gh-Pages</h2>
+        <h2>Gh-Pages 2</h2>
       </header>
     </div>
   );
