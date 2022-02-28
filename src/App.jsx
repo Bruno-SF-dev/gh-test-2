f87c8e5b4e5be7a51eb3dde874cf94af32311865
Bruno Santos <brunosantosfarias2@gmail.com>
2022-02-28 12:00:08 -0300

    add project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Gh-Pages</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
